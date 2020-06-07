d8f4047104e35b0a87b7538e5446c66144bb5218
redZeroZero <loicphilippe@hotmail.com>
2020-06-07 23:20:14 +0200

    Adding repository + data.sql

--- src/main/java/guru/springframework/spring5recipeapp/Spring5RecipeAppApplication.java
+++ src/main/java/guru/springframework/spring5recipeapp/Spring5RecipeAppApplication.java
@@ -1,13 +1,18 @@
 package guru.springframework.spring5recipeapp;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Spring5RecipeAppApplication {
 
+    Logger LOGGER = LoggerFactory.getLogger(Spring5RecipeAppApplication.class);
+
     public static void main(String[] args) {
         SpringApplication.run(Spring5RecipeAppApplication.class, args);
     }
+
 
 }
---
+++ src/main/java/guru/springframework/spring5recipeapp/repositories/UnitOfMesureRepository.java
@@ -0,0 +1,7 @@
+package guru.springframework.spring5recipeapp.repositories;
+
+import guru.springframework.spring5recipeapp.domain.UnitOfMeasure;
+import org.springframework.data.repository.CrudRepository;
+
+public interface UnitOfMesureRepository extends CrudRepository<UnitOfMeasure, Long> {
+}
